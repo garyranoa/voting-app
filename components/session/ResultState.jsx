b85a6f0132b9242abc442ef19e62814f93f1f2f1
garyranoa <angelkorn04@gmail.com>
2023-12-14 00:15:49 +0800

    Update ResultState.jsx

--- components/session/ResultState.jsx
+++ components/session/ResultState.jsx
@@ -5,20 +5,47 @@ import Link from "next/link";
 import { newRound } from "../../lib/firebase";
 
 function EndOfGame(option1, option2) {
+
+  const totalVotes = (option1.length > 0 ? option1[0].votes : 0) + (option2.length > 0 ? option2[0].votes : 0)
+  const percent1 = (option2[0].votes / totalVotes) * 100
+  const percent2 = (option1[0].votes / totalVotes) * 100
+
   return (
     <>
-    &nbsp;
-    {option1.length > 0 && (
-        <Title size="sm">
-          {option1[0].name} has total {option1[0].votes} vote(s)!
-        </Title>
-      )}
-      &nbsp;
-      {option2.length > 0 && (
-        <Title size="sm">
-          {option2[0].name} has total {option2[0].votes} vote(s)!
-        </Title>
-      )}
+
+<Title mb="md" size="h4">ENDING STATS</Title>
+      <Table
+        mr="auto"
+        ml="auto"
+        style={{ maxWidth: "350px" }}
+        verticalSpacing="xs"
+        horizontalSpacing="xs"
+        captionSide="bottom"
+        highlightOnHover
+      >
+        <caption></caption>
+        <thead>
+          <tr>
+            <th style={{ textAlign: "center" }}>OPTION</th>
+            <th style={{ textAlign: "center" }}>VOTING %</th>
+          </tr>
+        </thead>
+        <tbody>
+            {option2.length > 0 && (
+              <tr>
+              <td>{option2[0].name}</td>
+              <td>{option2[0].votes} / {`${percent1}%`}</td>
+            </tr>
+          )}
+
+          {option1.length > 0 && (
+              <tr>
+              <td>{option1[0].name}</td>
+              <td>{option1[0].votes} / {`${percent2}%`}</td>
+            </tr>
+          )}
+        </tbody>
+      </Table>
 
       <Title mt="xl" mb="xl">
         The End
@@ -69,10 +96,45 @@ function GameContinues(sessionId, dasher) {
   );
 }
 
+function ResultStats({ results }) {
+  return (
+    <>
+      <Title mb="md" size="h4">ROUND STATS</Title>
+      <Table
+        mr="auto"
+        ml="auto"
+        style={{ maxWidth: "350px" }}
+        verticalSpacing="xs"
+        horizontalSpacing="xs"
+        captionSide="bottom"
+        highlightOnHover
+      >
+        <caption></caption>
+        <thead>
+          <tr>
+            <th style={{ textAlign: "center" }}>OPTION</th>
+            <th style={{ textAlign: "center" }}>VOTING %</th>
+          </tr>
+        </thead>
+        <tbody>
+          {results.map((entry) => {
+            return (
+              <tr key={entry.option}>
+                <td>{entry.option}</td>
+                <td>{entry.percent}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+    </>
+  );
+}
+
 function Resultboard({ results }) {
   return (
     <>
-      <Title mb="md">Results</Title>
+      <Title mb="md" size="h2">RESULTS</Title>
       <Table
         mr="auto"
         ml="auto"
@@ -120,7 +182,7 @@ export default function ResultState({
       return {
         user: user,
         votes: round.votes[user].vote,
-        order: -voters[user].votes,
+        order: user
       };
     }),
     "order"
@@ -132,29 +194,50 @@ export default function ResultState({
 
   let option1TotalVotes = 0
   let option2TotalVotes = 0
+  let option1TRoundVotes = 0
+  let option2TRoundVotes = 0
   
+  const latestRound = rounds.at(-1);
   for (const round in rounds) {
     const roundInfo = rounds[round];
     for (const r in roundInfo) {
       if (r === 'votes') {
         for (const vote in roundInfo[r]) {
           let selected = roundInfo[r][vote].vote
-          if (selected === 'KEEP') {
+          if (selected === 'KILL') {
             option1TotalVotes++
           } else {
             option2TotalVotes++
+          }
+
+        
+          if (latestRound.number === roundInfo.number) {
+            if (selected === 'KILL') {
+              option1TRoundVotes++
+            } else {
+              option2TRoundVotes++
+            }
           }
         }
       }
     }
 }
-const option1 = [{name: "KEEP" , votes: option1TotalVotes}]
-const option2 = [{name: "KILL", votes: option2TotalVotes}]
+const option1 = [{name: "KILL" , votes: option1TotalVotes}]
+const option2 = [{name: "KEEP", votes: option2TotalVotes}]
+const percent1 = (option1TRoundVotes / 2) * 100
+const percent2 = (option2TRoundVotes / 2) * 100
+
+const stats = [{option: "KEEP", percent: `${percent2}%`}]
+stats.push({option: "KILL", percent: `${percent1}%`})
   
   return (
     <>
       {dasher === cookieCutter.get("username") && (
       <Resultboard results={results} />)}
+
+      {dasher === cookieCutter.get("username") && (
+      <ResultStats results={stats} />)}
+
       {isLastRound
         ? EndOfGame(option1, option2)
         : GameContinues(sessionId, dasher)}
