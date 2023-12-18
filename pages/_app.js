05d6afded20d934fbec632e0804551d8919844c2
garyranoa <angelkorn04@gmail.com>
2023-12-18 22:57:49 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -20,7 +20,7 @@ function MyApp({ Component, pageProps }) {
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" /> 
-      
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"/>
       
       <Layout>
         <Head>
