7ceb26cdce63f72f842a9e7c31df98b2c50f3f1f
markdvaldez <markdanovaldez@gmail.com>
2023-12-20 17:15:19 +0800

    updates

--- components/inputs/OptionsInput.jsx
+++ components/inputs/OptionsInput.jsx
@@ -18,6 +18,7 @@ export default function VotingOptionsInput() {
                 data={data}
                 label="Voting Options on this round"
                 placeholder="Pick voting options"
+                className="customSelect"
             />
         </>
     )
